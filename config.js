3cf636d2c5c444fdf797cffffb92920a565c4238
ducchacthe <92191853+ducchacthe@users.noreply.github.com>
2021-10-09 08:06:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ const CONFIG = {
     messDesc: 'messDesc',
     btnAccept: 'btnAccept',
     messLink: 'messLink', //link fb
-    bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
-    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
-    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
-    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
-    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
-    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+    bgLink: 'https://huyphadm9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
+    iputBgLink: 'https://huyphadm9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+    lookMeLink: 'https://huyphadm9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    soundLink: 'https://huyphadm9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huyphadm9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huyphadm9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
 }
